d9ac1b14bca849e44c3f59c04e0ff7bd13954a21
Tristan B. Kildaire <tristankildaire@gmail.com>
2013-09-01 17:45:29 +0200

    OpenProfile 1.0.2.0 (pre-beta)

--- OpenProfile.py
+++ OpenProfile.py
@@ -3,10 +3,15 @@ import tkinter
 'Below we start setting up strings'
 app_name = "OpenProfile"
 app_name_short = "OP"
-app_version_number = "1.0.1.0"
+app_version_number = "1.0.2.0"
 app_version_stableness = "pre-beta"
 app_version_complete = app_version_number + " " + app_version_stableness
-app_gui_window_title = app_name + " v" + app_version_number + " (gui)"
+'Some GUI stuff'
+app_gui_window_main_title = app_name + " v" + app_version_number + " (gui)"
+app_gui_window_about_title = "About " + app_name
+app_gui_window_main_size = "300x300"
+app_gui_window_about_size = "300x250"
+'End of GUI stuff'
 app_orginization = "DeavmiOSS"
 app_orginization_message = "This is free and open-source software from " + app_orginization + "."
 app_description = app_name + " is a free and open-source easy to use autobiography and biography creator written in Python."
@@ -18,6 +23,7 @@ app_license_description = app_name + " is registered under " + app_license + "."
 app_info_online_site_url = "http://bit.ly/getopenprofile"
 app_info_online_repository_url = "https://github.com/deavmi/OpenProfile"
 app_info_online_feedback_url = "https//deavmi.github.io/OpenProfile/feedback"
+app_info_online_wiki_url = "https://github.com/deavmi/OpenProfile/wiki"
 app_ui_console_welcomemsg = "Welcome to " + app_name + "!"
 'Getting everything referenced so that we can, "refer"-lol, to them when we need to a.k.a go to them-(the definitions)'
 def exit():
@@ -41,6 +47,7 @@ def about():
     print()
     print("License: " + app_license_url)
     print("Project site: " + app_info_online_site_url)
+    print("Wiki: " + app_info_online_wiki_url)
     print("Source code: " + app_info_online_repository_url)
     print("Feedback: " + app_info_online_feedback_url)
     print()
@@ -80,6 +87,7 @@ def help():
     print("about         Displays about info")
     print("credits       Displays credits")
     print("exit          Terminates the program")
+    print("gui           Starts OpenProfile in GUI mode")
     print("help          Displays list of commands")
     print("licenses      Displays licenses")
     print("q             Universally used for quitting")
@@ -96,6 +104,36 @@ def finish_autobiography():
 def begin():
     print()
     print("")
+def about_gui():
+    print()
+    print("Setting up the GUI...")
+    print("Setting window name...")
+    about = tkinter.Tk()
+    print("Setting window names... [Done]")
+    print("Setting window properties...")
+    print("Setting window size...")
+    about.geometry(app_gui_window_about_size)
+    print("Setting window title...")
+    about.title(app_gui_window_about_title)
+    print("Setting window title... [Done]")
+    print("Setting window properties... [Done]")
+    print("Setting up the GUI... [Done]")
+    'This above module is not correct I think yet, IDK how I want it be.'
+def gui():
+    print()
+    print("Setting up the GUI...")
+    print("Setting window name...")
+    main = tkinter.Tk()
+    print("Setting window names... [Done]")
+    print("Setting window properties...")
+    print("Setting window size...")
+    main.geometry(app_gui_window_main_size)
+    print("Setting window title...")
+    main.title(app_gui_window_main_title)
+    print("Setting window title... [Done]")
+    print("Setting window properties... [Done]")
+    print("Setting up the GUI... [Done]")
+    'This above module is not correct I think yet, IDK how I want it be.'
 def commandline():
     str = input(">>>")
     if str == "start":
@@ -115,6 +153,8 @@ def commandline():
         licenses()
     if str == "restart":
         app_start()
+    if str == "gui":
+        gui()
     if str == "":
         commandline()
 def app_start():
