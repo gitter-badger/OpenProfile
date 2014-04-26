32b9f4eed6016b7b63d47504925d55684efe707f
Tristan B. Kildaire <tristankildaire@gmail.com>
2014-04-26 13:26:28 +0200

    Yay.

--- OpenProfile.py
+++ OpenProfile.py
@@ -6,8 +6,8 @@ class AppInfo:
     name = "OpenProfile"
     name_short = "OP"
     version_number = "1.4.2.0"
-    version_stableness = "pre-beta"
-    version_complete = version_number + " " + version_stableness
+    version_phase = "pre-beta"
+    version_all = version_number + " " + version_phase
     orginization = "DeavmiOSS"
     orginization_message = "This is free, gratis and open-source software from " + orginization + "."
     description = name + " is a free, gratis and open-source easy to use autobiography and biography creator written in Python."
@@ -32,7 +32,8 @@ class AppInfo:
     wiki = "https://github.com/deavmi/OpenProfile/wiki"
     ui_console_welcomemsg = "Welcome to " + name + "!"
 class BuildConfig:
-	build_type = "OpenProfile"
+    '##### User build preferences to be changed according to your preference #####'
+    build_type = "OpenProfile"
 print("Setting application strings... [Done]")
 print("Creating objects...")
 app = AppInfo()
@@ -43,27 +44,26 @@ print("Creating object... [Done]")
 print(app.name + " build system is running...")
 print(app.name + " build type is being set...")
 '!!!!!!!!!!!! These are the only settings (below), so obviously change them if you need to !!!!!!!!!!!!'
-app_environment_build_type = "OpenProfile"
 'Below is for compatibility, because older version of Python want the user to use a captital "T" when importing tkinter.'
 app_environment_build_tkinter_import_mode = "modern"
 'Change this to "true" to enable logs inside the OpenProfile console.'
 app_environment_log2console_enabled = "true"
 'Change this to "true" to make OpenProfile start in GUI mode.'
-print(app.name + " build type set to: " + app_environment_build_type)
+print(app.name + " build type set to: " + buildconfig.build_type)
 '######'
-print("Checking for the selected build type's enviroment...")
+print("Checking for the selected build type's environment...")
 'Below are the different build types, their code is seperated by these "#"'
 '###### OpenProfile-system ######'
-if app_environment_build_type == "OpenProfile":
-    print("Checking for the selected build type's enviroment... [Done]")
-    print("Setting up the environemnt for the selected build type...")
+if buildconfig.build_type == "OpenProfile":
+    print("Checking for the selected build type's environment... [Done]")
+    print("Setting up the environment for the selected build type...")
     app_environment_build_machine_type = "[Windows/Mac OSX/GNU-Linux]"
     app_environment_gui_enabled = "true"
-    print("Setting up the environemnt for the selected build type... [Done]")
+    print("Setting up the environment for the selected build type... [Done]")
 '#################################'
 
 '###### OpenProfile-lite-system ######'
-if app_environment_build_type == "OpenProfile-lite":
+if buildconfig.build_type == "OpenProfile-lite":
     print("Checking for the selected build type's enviroment... [Done]")
     print("Setting up the environemnt for the selected build type...")
     app_environment_build_machine_type = "[Windows/Mac OSX/GNU-Linux (lite)]"
@@ -72,7 +72,7 @@ if app_environment_build_type == "OpenProfile-lite":
 '#################################'
 
 '###### OpenProfile-for-iOS-system ######'
-if app_environment_build_type == "OpenProfile-for-iOS":
+if buildconfig.build_type == "OpenProfile-for-iOS":
     print("Checking for the selected build type's enviroment... [Done]")
     print("Setting up the environemnt for the selected build type...")
     app_environment_build_machine_type = "[iOS]"
@@ -110,7 +110,7 @@ def about():
     print()
     print("-------------------------------------------------------")
     print("                      " + app.name)
-    print("                   v" + app.version_complete)
+    print("                   v" + app.version_all)
     print()
     print(app.description_part1)
     print(app.description_part2)
@@ -133,7 +133,7 @@ def credits():
     print("Credits of " + app.name + " v" + app.version_number + " .")
     print()
     print()
-    'I wanted three back slahes but apprently you always have to add +1 to the amount of slahes any way two slahes is enough'
+    'I wanted three back slashes but apprently you always have to add +1 to the amount of slashes any way two slahes is enough'
     print("\\\Developers")
     print()
     print("Deavmi - <http://bit.ly/thedeavmi>")
@@ -162,11 +162,11 @@ def credits():
     commandline()
 def license():
     print()
-    print(app_license_description)
+    print(app.license_description)
     print()
     print("All information regarding the NO WARRANTY disclamier and the redistribution disclaimer can be found in the license file online.")
     print()
-    print("Your copy of this license can be obtained here: " + app_license_url + " .")
+    print("Your copy of this license can be obtained here: " + app.license_url + " .")
     print()
     commandline()
 def licenses():
@@ -206,7 +206,7 @@ def help():
     print("restart       Restarts " + app.name)
     print("show c        Displays the " + app.name + " license")
     print("show w        Displays the " + app.name + " license")
-    print("start         Starts OpenProfile opperation")
+    print("start         Starts OpenProfile operation")
     print()
     commandline()
 def begin():
@@ -241,10 +241,20 @@ def begin():
     print()
     user_interests_list = input("List your interests (Comma format): ")
     print()
-    print("Just completing some cool things...")
+    print("1. Autobiography")
+    print("2. Biography")
+    print("3. Both of the above")
+    generation_option  = input("Which would you like to generate: ")
+    if generation_option == "1":
+        autobiography()
+    if generation_option == "2":
+        biography()
+    if generation_option == "3":
+        print("Coming soon!")
+    commandline()
     'If the person is below the age of 13, he is considered a kid/child (thanks mom ;P)'
     if user_time_age < "13":
-        user_time_age_type  = "kid"
+        user_time_age_type = "kid"
     'For all the ages below consider the person a teenager (thanks mom ;P, again)'
     if user_time_age == "13":
         user_time_age_type = "teenager"
@@ -264,14 +274,18 @@ def begin():
     if user_time_age > "19":
         user_time_age_type = "adult"
     'Not finished with all the Inputs and Outputs yet hay :P :D'
+def autobiography():
+    print()
+    print("-------------------------------------------------------")
     print()
     print("Below is your autobiography that " + app.name + " just generated:")
     print()
     'Put stuff here'
+def biography():
     print()
     print("-------------------------------------------------------")
     print()
-    print("Below is your biography that " +  app.name + " just generated:")
+    print("Below is your biography that " + app.name + " just generated:")
     print()
     'Put stuff here'
     commandline()
@@ -292,7 +306,7 @@ def about_gui():
        about.geometry(app_gui_window_about_size)
        print("Setting window size... [Done]")
        print("Setting window title...")
-       about.title(app_gui_window_about_title)
+       about.title("About " + app.name + " v" + app.version_all)
        print("Setting window title... [Done]")
        print("Setting window properties... [Done]")
        print("Setting up the GUI... [Done]")
@@ -324,13 +338,13 @@ def gui():
        main.geometry(app_gui_window_main_size)
        print("Setting window size... [Done]")
        print("Setting window title...")
-       main.title(app_gui_window_main_title)
+       main.title(app.name + " v" + app.version_all)
        print("Setting window title... [Done]")
        print("Setting window properties... [Done]")
        print("Setting up widgets...")
        'must finish this stuff here'
        label1_app_name = tkinter.Label(text=app.name)
-       label2_app_version = tkinter.Label(text=app.version_complete)
+       label2_app_version = tkinter.Label(text=app.version_all)
        button1_get_started = tkinter.Button(text="Get Started", command=begin)
        button2_about = tkinter.Button(text="About " + app.name, command=about_gui)
        button3_quit = tkinter.Button(text="Quit " + app.name, command=exit)
@@ -388,7 +402,7 @@ def commandline():
         commandline()
 def app_start():
     print("-------------------------------------------------------")
-    print(app.name + " v" + app.version_complete + " " + app_environment_build_machine_type)
+    print(app.name + " v" + app.version_all + " " + app_environment_build_machine_type)
     print(app.ui_console_welcomemsg)
     print("-------------------------------------------------------")
     print()
